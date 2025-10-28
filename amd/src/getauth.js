719a508b984c66bbb11a8cbc680e55e1c7f2a5e8
Tony Butler <tonyjbutler@gmail.com>
2025-10-28 18:20:31 +0000

    ITS-25020 mod_panopto: Fix progress bar and modernise javascript

--- amd/src/getauth.js
+++ amd/src/getauth.js
@@ -24,80 +24,108 @@
  * @copyright   2020 Tony Butler <a.butler4@lancaster.ac.uk>
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-define(
-    [
-        'jquery',
-        'core/ajax',
-        'core/notification',
-        'core/str',
-        'core/templates',
-        'core/modal_factory'
-    ],
-    function($, ajax, notification, str, templates, ModalFactory) {
 
-        /**
-         * Polls a web service method via AJAX for a Panopto auth url, then redirects to it.
-         *
-         * @param {number} contextId The context id of the Panopto instance.
-         * @param {number} panoptoId The instance id of the Panopto instance.
-         */
-        function getAuthUrl(contextId, panoptoId) {
-            var request = ajax.call([{
-                methodname: 'mod_panopto_get_auth',
-                args: {
-                    contextid: contextId,
-                    panoptoid: panoptoId
-                }
-            }]);
+import Ajax from 'core/ajax';
+import Modal from 'core/modal';
+import {getString} from 'core/str';
+import Templates from 'core/templates';
 
-            request[0].done(function(authUrl) {
-                if (authUrl) {
-                    $('#panopto_progress_bar').val(100);
-                    window.location.replace(authUrl);
-                }
-                // Wait a sec ...
-                setTimeout(function() {
-                    getAuthUrl(contextId, panoptoId);
-                }, 1000);
-            }).fail(function(exception) {
-                str.get_string(exception.errorcode, 'mod_panopto').done(function(message) {
-                    $('#panopto_progress').replaceWith(message);
-                    $('.mod_panopto_progress').addClass('alert-warning p-2');
-                }).fail(notification.exception);
-            });
-        }
+/**
+ * Init function.
+ *
+ * @param {number} contextId The context id of the Panopto instance.
+ * @param {number} panoptoId The instance id of the Panopto instance.
+ * @return {Promise} A promise.
+ */
+export const init = async(contextId, panoptoId) => {
+    const templateContext = {
+        id: panoptoId,
+        idnumber: 'panopto_progress',
+        width: 400,
+        "class": '',
+        value: 0,
+        error: 0,
+    };
+    const modal = await Modal.create({
+        title: await getString('preparing', 'mod_panopto'),
+        body: await Templates.render('core/progress_bar', templateContext),
+        isVerticallyCentered: true,
+        removeOnClose: true,
+        show: true,
+    });
+    modal.getRoot().find('.modal-dialog').css({'width': '400px'});
 
-        return {
-            /**
-             * Init function.
-             *
-             * @param {number} contextId The context id of the Panopto instance.
-             * @param {number} panoptoId The instance id of the Panopto instance.
-             */
-            init: function(contextId, panoptoId) {
-                // Start polling for an auth url.
-                getAuthUrl(contextId, panoptoId);
+    const progress = document.getElementById('panopto_progress'),
+        progressBar = document.getElementById('panopto_progress_bar');
+    let intervalId;
 
-                var context = {
-                    id: 'panopto_progress',
-                    width: 350
-                },
-                    trigger = $('#panopto_info');
-
-                str.get_string('preparing', 'mod_panopto').done(function(title) {
-                    ModalFactory.create({
-                        title: title,
-                        body: templates.render('mod_panopto/progress', context, ''),
-                        type: ModalFactory.types.DEFAULT,
-                        large: false
-                    }, trigger).done(function(modal) {
-                        modal.getRoot().find('.modal-dialog').css({'width': '400px', 'margin-top': '20rem'});
-                        // Launch modal.
-                        trigger.trigger('click');
-                    }).fail(notification.exception);
-                }).fail(notification.exception);
+    /**
+     * Increments the progress bar at an exponentially decreasing rate.
+     */
+    const incProgress = () => {
+        let value = progressBar.getAttribute('value');
+        intervalId = setInterval(() => {
+            if (progressBar.getAttribute('value') < 100 && value < 99.9) {
+                value = value + (1 - value / 100);
+                const increment = new CustomEvent('update', {
+                    detail: {
+                        percent: value,
+                    },
+                });
+                progress.dispatchEvent(increment);
+            } else {
+                clearInterval(intervalId);
             }
-        };
+        }, 100);
+    };
+    incProgress();
 
-    }
-);
+    /**
+     * Polls a web service method via AJAX for a Panopto auth url, then redirects to it.
+     *
+     * @param {number} contextId The context id of the Panopto instance.
+     * @param {number} panoptoId The instance id of the Panopto instance.
+     * @return {Promise} A promise.
+     */
+    const getAuthUrl = async(contextId, panoptoId) => {
+        const request = Ajax.call([{
+            methodname: 'mod_panopto_get_auth',
+            args: {
+                contextid: contextId,
+                panoptoid: panoptoId,
+            },
+        }]);
+
+        request[0].then((authUrl) => {
+            if (typeof authUrl === 'string') {
+                const done = new CustomEvent('update', {
+                    detail: {
+                        percent: 100,
+                    },
+                });
+                progress.dispatchEvent(done);
+                window.location.replace(authUrl);
+            }
+            // Wait a sec ...
+            setTimeout(() => {
+                getAuthUrl(contextId, panoptoId);
+            }, 1000);
+        }).catch(async(exception) => {
+            const message = await getString(exception.errorcode, 'mod_panopto');
+            const failed = new CustomEvent('update', {
+                detail: {
+                    percent: 0,
+                    error: 1,
+                    message: message,
+                },
+            });
+            progress.dispatchEvent(failed);
+            clearInterval(intervalId);
+            const progressStatus = document.getElementById('panopto_progress_status');
+            progressStatus.classList.remove('text-truncate');
+        });
+    };
+
+    // Start polling for an auth url.
+    await getAuthUrl(contextId, panoptoId);
+};
